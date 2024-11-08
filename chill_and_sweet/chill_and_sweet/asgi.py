656b05eca50cd02a1efff35393362a8376678570
UlisesRdgz <ulises.rdgz@ciencias.unam.mx>
2024-11-07 21:17:38 -0600

    chore: add initial project setup with README and requirements

---
+++ chill_and_sweet/chill_and_sweet/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for chill_and_sweet project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chill_and_sweet.settings')
+
+application = get_asgi_application()
